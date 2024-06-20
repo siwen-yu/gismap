f74a9d01986834bfc1cc87965b0230d7a6bbf900
yusiwen <18702515518@163.com>
2024-06-20 17:03:14 +0800

    Delete docker/deploy.sh

--- docker/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker build -t gismap-backend:1.0.0 backend
-docker tag gismap-backend:1.0.0 registry.cn-hangzhou.aliyuncs.com/yusiwen12138/gismap-backend:1.0.0
-docker push registry.cn-hangzhou.aliyuncs.com/yusiwen12138/gismap-backend:1.0.0
-
-docker build -t gismap-frontend:1.0.0 frontend
-docker tag gismap-frontend:1.0.0 registry.cn-hangzhou.aliyuncs.com/yusiwen12138/gismap-frontend:1.0.0
-docker push registry.cn-hangzhou.aliyuncs.com/yusiwen12138/gismap-frontend:1.0.0
